4a44cfc2b717b379dc247c51fe59b2cedf6e64c4
Nvertigo <nvertigo67@gmail.com>
2020-02-18 11:03:48 +0100

    current.pick.sh: "webview_80.0.3987.99".

--- current.pick.sh
+++ current.pick.sh
@@ -77,5 +77,8 @@ safer-repopick 267444 -P frameworks/base/
 # Refactor AUDIO_BECOMING_NOISY broadcast handling
 safer-repopick 268138 -P packages/apps/Eleven/
 
+# webview_80.0.3987.99
+safer-repopick -t webview_80.0.3987.99
+
 # Longshot
 ${LOCAL_MANIFEST}/current.pick.git.sh
