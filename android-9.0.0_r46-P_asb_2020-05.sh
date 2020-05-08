ff162e2365f0c31f2db0253dcecb306c4a1ac533
Nvertigo <nvertigo67@gmail.com>
2020-05-08 10:17:26 +0200

    Patch up: P_asb_2020-05.

--- android-9.0.0_r46-P_asb_2020-05.sh
+++ android-9.0.0_r46-P_asb_2020-05.sh
@@ -20,7 +20,7 @@ safer-repopick() {
     fi
 }
 
-safer-repopick -t P_asb_2020-04 -e 272359,272360,272361,272362,272560
-safer-repopick 272359 272360 -P frameworks/av
-safer-repopick 272361 272362 -P frameworks/base/
-safer-repopick 272560 -P build/make
+safer-repopick -t P_asb_2020-05 -e 275016,275017,275019,275020,275021,275022,275023,275024,275025,275027
+safer-repopick 275016 275017 -P frameworks/av/
+safer-repopick 275019 275020 275021 275022 275023 275024 275025 -P frameworks/base/
+safer-repopick 275027 -P packages/apps/Settings
